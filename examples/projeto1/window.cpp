ac7c767eb8404d841434a7ba9417f37d3f02ab6b
negraop <pedro.negrao12@gmail.com>
2024-11-05 23:31:17 -0300

    criação da janela

--- examples/projeto1/window.cpp
+++ examples/projeto1/window.cpp
@@ -15,21 +15,27 @@ void Window::onPaint() {
 }
 
 void Window::showTutorialWindow() {
-    // MENU
-    ImGui::SetNextWindowSize(ImVec2(300, 100));
-    auto flags{ImGuiWindowFlags_NoResize};
-    ImGui::Begin("Tutorial", nullptr, flags);
-    {
-        bool save{};
-        static bool showCompliment{};  // Hold state
+  ImVec2 windowSize(300, 100);
 
-        if (showCompliment) {
-            ImGui::Text("Bem-vindo ao Planeta X");
-            ImGui::Text("Para ");
-        }
-    }
+  auto const &windowSettings{getWindowSettings()};
+  float screenWidth = static_cast<float>(windowSettings.width);
+  float screenHeight = static_cast<float>(windowSettings.height);
 
-    ImGui::End();
+  ImVec2 windowPos((screenWidth - windowSize.x) / 2.0f,
+                   (screenHeight - windowSize.y) / 2.0f);
+
+  ImGui::SetNextWindowPos(windowPos, ImGuiCond_Always);
+
+  // MENU
+  ImGui::SetNextWindowSize(windowSize);
+  auto flags{ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse};
+  ImGui::Begin("Tutorial", &m_showTutorialWindow, flags);
+  {
+    ImGui::Text("Bem-vindo ao Planeta X");
+    ImGui::Text("[Escrever aqui o tutorial]");
+  }
+
+  ImGui::End();
 }
 
 void Window::onPaintUI() {
@@ -48,134 +54,21 @@ void Window::onPaintUI() {
 
     // Menu
     {
-      bool tutorialSelected{};
       if (ImGui::BeginMenuBar()) {
         if (ImGui::BeginMenu("Ajuda")) {
-          ImGui::MenuItem("Tutorial", nullptr, &tutorialSelected);
+          if (ImGui::MenuItem("Tutorial")) {
+            m_showTutorialWindow = true;
+          }
           ImGui::EndMenu();
         }
         ImGui::EndMenuBar();
       }
 
-      if (tutorialSelected) {
+      if (m_showTutorialWindow) {
         showTutorialWindow();
       }
     }
 
     ImGui::End();
   }
-
-  // // Our own ImGui widgets go below
-  // {
-  //   // Window begin
-  //   ImGui::Begin("Hello, First App!");
-
-  //   // Static text
-  //   auto const &windowSettings{getWindowSettings()};
-  //   ImGui::Text("Current window size: %dx%d (in windowed mode)",
-  //               windowSettings.width, windowSettings.height);
-
-  //   // Slider from 0.0f to 1.0f
-  //   static float f{};
-  //   ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
-
-  //   // ColorEdit to change the clear color
-  //   ImGui::ColorEdit3("clear color", m_clearColor.data());
-
-  //   // More static text
-  //   ImGui::Text("Application average %.3f ms/frame (%.1f FPS)",
-  //               1000.0 / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-
-
-  //   // BOTÕES
-  //   // 100x50 button
-  //   if (ImGui::Button("Press me!", ImVec2(100, 50))) {
-  //       fmt::print("1st button pressed.\n");
-  //   }
-
-  //   // Nx50 button, where N is the remaining width available
-  //   ImGui::Button("Press me!", ImVec2(-1, 50));
-
-  //   // See also IsItemHovered, IsItemActive, etc
-  //   if (ImGui::IsItemClicked()) {
-  //       fmt::print("2nd Button pressed.\n");
-  //   }
-
-
-  //   // CHECKBOX
-  //   static bool enabled{true};
-  //   ImGui::Checkbox("Some option", &enabled);
-  //   fmt::print("The checkbox is {}\n", enabled ? "enabled" : "disabled");
-
-
-  //   // COMBOBOX
-  //   static std::size_t currentIndex{};
-  //   std::vector comboItems{"AAA", "BBB", "CCC"};
-
-  //   if (ImGui::BeginCombo("Combo box", comboItems.at(currentIndex))) {
-  //       for (auto index{0U}; index < comboItems.size(); ++index) {
-  //           bool const isSelected{currentIndex == index};
-  //           if (ImGui::Selectable(comboItems.at(index), isSelected))
-  //           currentIndex = index;
-
-  //           // Set the initial focus when opening the combo (scrolling + keyboard
-  //           // navigation focus)
-  //           if (isSelected)
-  //           ImGui::SetItemDefaultFocus();
-  //       }
-  //       ImGui::EndCombo();
-  //   }
-
-  //   fmt::print("Selected combo box item: {}\n", comboItems.at(currentIndex));
-
-
-
-  //   // MENU
-  //   ImGui::SetNextWindowSize(ImVec2(300, 100));
-  //   auto flags{ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoResize};
-  //   ImGui::Begin("Window with menu", nullptr, flags);
-  //   {
-  //       bool save{};
-  //       static bool showCompliment{};  // Hold state
-
-  //       // Menu Bar
-  //       if (ImGui::BeginMenuBar()) {
-  //           // File menu
-  //           if (ImGui::BeginMenu("File")) {
-  //               ImGui::MenuItem("Save", nullptr, &save);
-  //               ImGui::EndMenu();
-  //           }
-  //           // View menu
-  //           if (ImGui::BeginMenu("View")) {
-  //               ImGui::MenuItem("Show Compliment", nullptr, &showCompliment);
-  //               ImGui::EndMenu();
-  //           }
-  //           ImGui::EndMenuBar();
-  //       }
-
-  //       if (save) {
-  //           // Save file...
-  //       }
-
-  //       if (showCompliment) {
-  //           ImGui::Text("You're a beautiful person.");
-  //       }
-  //   }
-
-  //   ImGui::End();
-
-    
-
-  //   // MAIS SLIDERS
-  //   static std::array pos2d{0.0f, 0.0f};
-  //   ImGui::SliderFloat2("2D position", pos2d.data(), 0.0, 50.0);
-
-  //   static std::array pos3d{0.0f, 0.0f, 0.0f};
-  //   ImGui::SliderFloat3("3D position", pos3d.data(), -1.0, 1.0);
-
-    
-    
-  //   // Window end
-  //   ImGui::End();
-  // }
 }
