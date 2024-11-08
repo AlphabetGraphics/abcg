afe2b53e74655ecefb65ac5371e64474e36ede1a
negraop <pedro.negrao12@gmail.com>
2024-11-07 22:14:43 -0300

    add astronauta

--- examples/asteroids4/ship.cpp
+++ examples/asteroids4/ship.cpp
@@ -21,53 +21,80 @@ void Ship::create(GLuint program) {
 
   // clang-format off
   std::array positions{
-      // Ship body
-      glm::vec2{-02.5f, +12.5f}, glm::vec2{-15.5f, +02.5f},
-      glm::vec2{-15.5f, -12.5f}, glm::vec2{-09.5f, -07.5f},
-      glm::vec2{-03.5f, -12.5f}, glm::vec2{+03.5f, -12.5f},
-      glm::vec2{+09.5f, -07.5f}, glm::vec2{+15.5f, -12.5f},
-      glm::vec2{+15.5f, +02.5f}, glm::vec2{+02.5f, +12.5f},
+      // Astronaut Body
+      glm::vec2{-04.0f, +7.0f},
+      glm::vec2{-07.0f, +4.0f},
+      glm::vec2{-07.0f, +2.0f},
+      glm::vec2{-03.0f, 0.0f},
+      glm::vec2{-05.0f, -2.0f},
+      glm::vec2{-07.0f, -2.0f},
+      glm::vec2{-07.0f, -3.0f},
+      glm::vec2{-05.0f, -3.0f},
+      glm::vec2{-05.0f, -4.0f},
+      glm::vec2{-03.0f, -6.0f},
+      glm::vec2{-05.0f, -6.0f},
+      glm::vec2{-05.0f, -7.0f},
+      glm::vec2{-02.0f, -7.0f},
+      glm::vec2{-02.0f, -6.0f},
+      glm::vec2{+02.0f, -6.0f},
+      glm::vec2{+02.0f, -7.0f},
+      glm::vec2{+05.0f, -7.0f},
+      glm::vec2{+05.0f, -6.0f},
+      glm::vec2{+03.0f, -6.0f},
+      glm::vec2{+05.0f, -4.0f},
+      glm::vec2{+05.0f, -3.0f},
+      glm::vec2{+07.0f, -3.0f},
+      glm::vec2{+07.0f, -2.0f},
+      glm::vec2{+05.0f, -2.0f},
+      glm::vec2{+03.0f, 0.0f},
+      glm::vec2{+07.0f, +2.0f},
+      glm::vec2{+07.0f, +4.0f},
+      glm::vec2{+04.0f, +7.0f},
 
-      // Cannon (left)
-      glm::vec2{-12.5f, +10.5f}, glm::vec2{-12.5f, +04.0f},
-      glm::vec2{-09.5f, +04.0f}, glm::vec2{-09.5f, +10.5f},
-
-      // Cannon (right)
-      glm::vec2{+09.5f, +10.5f}, glm::vec2{+09.5f, +04.0f},
-      glm::vec2{+12.5f, +04.0f}, glm::vec2{+12.5f, +10.5f},
-      
       // Thruster trail (left)
-      glm::vec2{-12.0f, -07.5f}, 
-      glm::vec2{-09.5f, -18.0f}, 
-      glm::vec2{-07.0f, -07.5f},
+      glm::vec2{-04.0f, -03.0f}, 
+      glm::vec2{-06.0f, -06.0f}, 
+      glm::vec2{-03.0f, -04.0f},
 
       // Thruster trail (right)
-      glm::vec2{+07.0f, -07.5f}, 
-      glm::vec2{+09.5f, -18.0f}, 
-      glm::vec2{+12.0f, -07.5f},
+      glm::vec2{+03.0f, -04.0f}, 
+      glm::vec2{+06.0f, -06.0f}, 
+      glm::vec2{+04.0f, -03.0f},
       };
 
   // Normalize
   for (auto &position : positions) {
-    position /= glm::vec2{15.5f, 15.5f};
+    position /= glm::vec2{7.0f, 7.0f};
   }
 
-  std::array const indices{0, 1, 3,
-                           1, 2, 3,
-                           0, 3, 4,
-                           0, 4, 5,
-                           9, 0, 5,
-                           9, 5, 6,
-                           9, 6, 8,
-                           8, 6, 7,
-                           // Cannons
-                           10, 11, 12,
-                           10, 12, 13,
-                           14, 15, 16,
-                           14, 16, 17,
-                           // Thruster trails
-                           18, 19, 20,
-                           21, 22, 23};
+  std::array const indices{
+      0, 1, 2,
+                            0, 2, 3,
+                            0, 3, 24,
+                            0, 24, 27,
+                            24, 25, 27,
+                            25, 26, 27,
+                            3, 4, 23,
+                            3, 23, 24,
+                            4, 5, 6,
+                            4, 6, 7,
+                            4, 8, 19,
+                            4, 19, 23,
+                            20, 21, 23,
+                            21, 22, 23,
+                            8, 9, 18,
+                            8, 18, 19,
+                            9, 10, 11,
+                            9, 11, 12,
+                            9, 12, 13,
+                            14, 15, 18,
+                            15, 16, 18,
+                            16, 17, 18,
+
+                            // Thruster trails
+                            28, 29, 30,
+                            31, 32, 33,
+  };
   // clang-format on
 
   // Generate VBO
@@ -131,14 +158,14 @@ void Ship::paint(const GameData &gameData) {
       // 50% transparent
       abcg::glUniform4f(m_colorLoc, 1, 1, 1, 0.5f);
 
-      abcg::glDrawElements(GL_TRIANGLES, 14 * 3, GL_UNSIGNED_INT, nullptr);
+      abcg::glDrawElements(GL_TRIANGLES, 24 * 3, GL_UNSIGNED_INT, nullptr);
 
       abcg::glDisable(GL_BLEND);
     }
   }
 
   abcg::glUniform4fv(m_colorLoc, 1, &m_color.r);
-  abcg::glDrawElements(GL_TRIANGLES, 12 * 3, GL_UNSIGNED_INT, nullptr);
+  abcg::glDrawElements(GL_TRIANGLES, 22 * 3, GL_UNSIGNED_INT, nullptr);
 
   abcg::glBindVertexArray(0);
 
